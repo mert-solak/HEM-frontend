e59dba63b68c4c8ab5eb35b03bd6ab0cf816dba3
ubuntu-work <solak.mert@hotmail.com>
2021-04-23 08:59:14 -0700

    feat(src/pages/equipments/*): equipments pages created

---
+++ src/app/pages/equipments/equipments.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EquipmentsComponent } from '@pages/equipments/equipments.component';
+
+@NgModule({
+  declarations: [EquipmentsComponent],
+  imports: [CommonModule],
+})
+export class EquipmentsModule {}
